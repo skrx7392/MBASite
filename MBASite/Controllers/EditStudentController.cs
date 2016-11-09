eb4a8a6ed59a9cf7dff7ebeb0c3b0131d4ce6fab
Sai Krishna Podduturi <skrx7392@gmail.com>
2016-11-09 12:26:32 -0500

    added edit student details

--- MBASite/Controllers/EditStudentController.cs
+++ MBASite/Controllers/EditStudentController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MBASite.Helpers;
 using MBASite.ViewModels;
+using MBASite.Models;
 
 namespace MBASite.Controllers
 {
@@ -28,6 +29,34 @@ namespace MBASite.Controllers
             }
             
             return View(Students);
+        }
+
+        [HttpPost]
+        public ActionResult StudentList(StudentId studentId)
+        {
+            StudentData studentData = populateData(studentId);
+            TempData["studentData"] = new StudentData();
+            return RedirectToAction("EditStudentDetails", "EditStudentDetails");
+        }
+
+        public StudentData populateData(StudentId studentId)
+        {
+            StudentData studentData = new ViewModels.StudentData();
+            StudentDetails studentDetails = StaticVariables.StudentDetails.FirstOrDefault(p => p.Id == studentId.Id);
+            studentData.Id = studentDetails.Id;
+            studentData.Address = studentDetails.Address;
+            studentData.Comments = studentDetails.Comments;
+            studentData.Concentration = StaticVariables.Programs.FirstOrDefault(p => p.Id == studentDetails.ProgramId).Name;
+            studentData.FirstName = studentDetails.FirstName;
+            studentData.GMATScore = studentDetails.GMATScore.HasValue ? studentDetails.GMATScore.Value : 0;
+            studentData.GPA = studentDetails.GPA.HasValue ? studentDetails.GPA.Value : 0;
+            studentData.GREScore = studentDetails.GREScore.HasValue ? studentDetails.GREScore.Value : 0;
+            studentData.LastName = studentDetails.LastName;
+            studentData.NonUCMOEmailId = studentDetails.AlternateEmail;
+            studentData.PhoneNumber = studentDetails.PhoneNumber;
+            studentData.ProgramEntryDate = studentDetails.CreatedDate;
+            studentData.UCMOEmailId = studentDetails.Email;
+            return studentData;
         }
     }
 }
