47e2d12d458e505377770902ad9f1bc53a9d3d5b
VishalReddyRamini <vishalreddy.ramini@outlook.com>
2016-11-10 12:28:43 -0500

    viewsUpdated

--- MBASite/ViewModels/StudentData.cs
+++ MBASite/ViewModels/StudentData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MBASite.ViewModels
 {
@@ -13,12 +14,14 @@ namespace MBASite.ViewModels
         public string Concentration { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
-        public string ProgramEntryDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ProgramEntryDate { get; set; }
         public int GMATScore { get; set; }
         public int GREScore { get; set; }
         public decimal GPA { get; set; }
         public string UCMOEmailId { get; set; }
         public string NonUCMOEmailId { get; set; }
+       
         public string Comments { get; set; }
     }
 }
