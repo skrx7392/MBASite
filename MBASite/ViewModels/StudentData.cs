1512232652e0b6f2c5c8afc188d317e53497555e
Sekhar <Sekhar@DESKTOP-A0558KS>
2016-11-14 02:32:41 -0500

    check

--- MBASite/ViewModels/StudentData.cs
+++ MBASite/ViewModels/StudentData.cs
@@ -9,6 +9,7 @@ namespace MBASite.ViewModels
     public class StudentData
     {
         public int Id { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Concentration { get; set; }
@@ -16,10 +17,20 @@ namespace MBASite.ViewModels
         [Required(ErrorMessage = "Telephone Number Required")]
         [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Entered phone format is not valid.")]
         public string PhoneNumber { get; set; }
+       
+        
+        [Required(ErrorMessage = "Date field should not be Empty")]
+       
         [DataType(DataType.Date)]
         public DateTime ProgramEntryDate { get; set; }
+        [Required]
+        [Range(200, 800)]
         public int GMATScore { get; set; }
+        [Required]
+        [Range(270,340)]
         public int GREScore { get; set; }
+        [Required]
+        [Range(2, 4)]
         public decimal GPA { get; set; }
         [RegularExpression(".+@.+\\..+", ErrorMessage = "Please Enter Correct Email Address")]
         public string UCMOEmailId { get; set; }
